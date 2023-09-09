a3936b687eeafb4d43cedec0c04f3abb0b582701
ethanburmane <ethanburmane@gmail.com>
2023-09-09 13:50:52 -0400

    got two of the camera streams to open on start up

--- UI/camera_stream.py
+++ UI/camera_stream.py
@@ -52,9 +52,9 @@ class CameraStreamWidget(QWidget):
         # put address from local machine into host command
 
         # gstreamer commands for starting stream on the pi
-        launch_stream1 = "gst-launch-1.0 -v v4l2src device=/dev/video8 ! video/x-h264, width=1920,height=1080! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=10.0.0.101 port=5601 sync=false buffer-size=1048576"
-        launch_stream2 = "gst-launch-1.0 -v v4l2src device=/dev/video4 ! video/x-h264, width=1920,height=1080! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=10.0.0.102 port=5600 sync=false buffer-size=1048576"
-        launch_stream3 = "gst-launch-1.0 -v v4l2src device=/dev/video0 ! image/jpeg, width=1920,height=1080, framerate=30/1 ! jpegparse ! queue max-size-buffers=100 ! rtpjpegpay ! udpsink host=10.0.0.102 port=5602 sync=false buffer-size=1048576"
+        launch_stream1 = "gst-launch-1.0 -v v4l2src device=/dev/video8 ! video/x-h264, width=1920,height=1080! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=10.0.0.101 port=5600 sync=false buffer-size=1048576"
+        launch_stream2 = "gst-launch-1.0 -v v4l2src device=/dev/video4 ! video/x-h264, width=1920,height=1080! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=10.0.0.101 port=5601 sync=false buffer-size=1048576"
+        launch_stream3 = "gst-launch-1.0 -v v4l2src device=/dev/video0 ! image/jpeg, width=1920,height=1080, framerate=30/1 ! jpegparse ! queue max-size-buffers=100 ! rtpjpegpay ! udpsink host=10.0.0.101 port=5602 sync=false buffer-size=1048576"
 
         # starting subprocesses for each stream
         # subprocess.Popen(launch_stream1, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1, universal_newlines=True)
@@ -62,12 +62,11 @@ class CameraStreamWidget(QWidget):
         # subprocess.run(launch_stream3, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1, universal_newlines=True)
 
         # gstreamer pipeline for receiving stream 
-        receive_stream1 = "gst-launch-1.0 udpsrc port=5601 ! application/x-rtp ! rtpjitterbuffer ! rtph264depay ! avdec_h264 ! videoconvert ! autovideosink sync=false"
-        receive_stream2 = "gst-launch-1.0 -v v4l2src device=/dev/video4 ! video/x-h264, width=1920,height=1080! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=10.0.0.102 port=5600 sync=false buffer-size=1048576"
-        receive_stream3 = "gst-launch-1.0 -v v4l2src device=/dev/video0 ! image/jpeg, width=1920,height=1080, framerate=30/1 ! jpegparse ! queue max-size-buffers=100 ! rtpjpegpay ! udpsink host=10.0.0.102 port=5602 sync=false buffer-size=1048576"
+        receive_stream1 = "gst-launch-1.0 udpsrc port=5600 ! application/x-rtp ! rtpjitterbuffer ! rtph264depay ! avdec_h264 ! videoconvert ! autovideosink sync=false"
+        receive_stream2 = "gst-launch-1.0 udpsrc port=5601 ! application/x-rtp ! rtpjitterbuffer ! rtph264depay ! avdec_h264 ! videoconvert ! autovideosink sync=false"
+        # receive_stream3 = "gst-launch-1.0 -v v4l2src device=/dev/video0 ! image/jpeg, width=1920,height=1080, framerate=30/1 ! jpegparse ! queue max-size-buffers=100 ! rtpjpegpay ! udpsink host=10.0.0.101 port=5602 sync=false buffer-size=1048576"
         
         # starting subprocesses for each stream
-        self.camera_process = subprocess.Popen(receive_stream1, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        # subprocess.Popen(receive_stream1, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1, universal_newlines=True)
-        # subprocess.Popen(receive_stream2, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1, universal_newlines=True)
-        # subprocess.Popen(receive_stream3, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1, universal_newlines=True)
+        self.stream1_process = subprocess.Popen(receive_stream1, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        self.stream2_process = subprocess.Popen(receive_stream2, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # self.stream3_process = subprocess.Popen(receive_stream3, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
