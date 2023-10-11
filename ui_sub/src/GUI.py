31b7a679095d0fb4f0584b5def35f596db3146e2
Ben Boardley <bboard1202@gmail.com>
2023-10-11 19:53:04 -0400

    UI as ros package, subscribers can now update UI

---
+++ ui_sub/src/GUI.py
@@ -0,0 +1,304 @@
+#!/usr/bin/env python3
+# Frontend UI for ROV X16
+# Written and edited by the X16 Software Team 
+# Ethan Burmane (eburmane@purdue.edu), Caden Brennan (brenna51@purdue.edu)
+# Last updated: 08/27/2023
+
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+import command as command
+
+#import camera_stream as stream
+import readouts as readouts
+import controls as controls 
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initMainWindow()
+
+    def initMainWindow(self):
+
+        # setting the main window properties
+        self.showMaximized()
+        self.setWindowTitle("ROV X16")
+        self.setStyleSheet('''
+            QMainWindow {
+                background-color: #2C2C2C;
+            }
+            ''')
+
+        # adding the controls dock to the main window
+        self.controls_widget = controls.ControlsWidget()
+        self.addDockWidget(Qt.LeftDockWidgetArea, self.controls_widget)
+
+        # adding the readout dock to the main window 
+        self.readout_widget = readouts.ReadoutsWidget()
+        self.addDockWidget(Qt.RightDockWidgetArea, self.readout_widget)
+
+        # adding the stream widget to the main window
+        #self.camera_stream_widget = stream.CameraStreamWidget()
+        #self.setCentralWidget(self.camera_stream_widget)
+
+    # This is the event handlers for key press and key release events.
+    # This contains the directional control keys and the tool keys.
+
+    def keyPressEvent(self, event):
+
+        # checking for directional pitch key press events (W, A, S, D)
+        if event.key() == Qt.Key_W:
+            print("Tilt Forward")
+            self.controls_widget.tilt_forward.setDown(True)
+            self.controls_widget.tilt_forward.setStyleSheet('''
+                QPushButton {
+                    background-color: #989898;
+                    color: black;
+                }
+                ''')
+            self.controls_widget.tilt_forward.repaint()
+        elif event.key() == Qt.Key_A:
+            print("Tilt Left")
+            self.controls_widget.tilt_left.setDown(True)
+            self.controls_widget.tilt_left.setStyleSheet('''
+                QPushButton {
+                    background-color: #989898;
+                    color: black;
+                }
+                ''')
+            self.controls_widget.tilt_left.repaint()
+        elif event.key() == Qt.Key_S:
+            print("Tilt Backwards")
+            self.controls_widget.tilt_backwards.setDown(True)
+            self.controls_widget.tilt_backwards.setStyleSheet('''
+                QPushButton {
+                    background-color: #989898;
+                    color: black;
+                }
+                ''')
+            self.controls_widget.tilt_backwards.repaint()
+        elif event.key() == Qt.Key_D:
+            print("Tilt Right")
+            self.controls_widget.tilt_right.setDown(True)
+            self.controls_widget.tilt_right.setStyleSheet('''
+                QPushButton {
+                    background-color: #989898;
+                    color: black;
+                }
+                ''')
+            self.controls_widget.tilt_right.repaint()
+
+        # checking for directional yaw key press events (Q, E)
+        elif event.key() == Qt.Key_Q:
+            print("Yaw Left")
+            self.controls_widget.yaw_left.setDown(True)
+            self.controls_widget.yaw_left.setStyleSheet('''
+                QPushButton {
+                    background-color: #989898;
+                    color: black;
+                }
+                ''')
+            self.controls_widget.yaw_left.repaint()
+        elif event.key() == Qt.Key_E:
+            print("Yaw Right")
+            self.controls_widget.yaw_right.setDown(True)
+            self.controls_widget.yaw_right.setStyleSheet('''
+                QPushButton {
+                    background-color: #989898;
+                    color: black;
+                }
+                ''')
+            self.controls_widget.yaw_right.repaint()
+
+        # checking for ascent and descent key press events (R, F)
+        elif event.key() == Qt.Key_R:
+            print("Ascend")
+            self.controls_widget.ascend.setDown(True)
+            self.controls_widget.ascend.setStyleSheet('''
+                QPushButton {
+                    background-color: #989898; 
+                    color: black;
+                }
+                ''')
+            self.controls_widget.ascend.repaint()
+        elif event.key() == Qt.Key_F:
+            print("Descend")
+            self.controls_widget.descend.setDown(True)
+            self.controls_widget.descend.setStyleSheet('''
+                QPushButton {
+                    background-color: #989898; 
+                    color: black;
+                }
+                ''')
+            self.controls_widget.descend.repaint()
+
+        # checking for tool key press events (6, 7, 8, 9)
+        elif event.key() == Qt.Key_6:
+            print("Tool 1")
+            self.controls_widget.tool1.setDown(True)
+            self.controls_widget.tool1.setStyleSheet('''
+                QPushButton {
+                    background-color: #989898; 
+                    color: black;
+                }
+                ''')
+            self.controls_widget.tool1.repaint()
+
+        elif event.key() == Qt.Key_7:
+            print("Tool 2")
+            self.controls_widget.tool2.setDown(True)
+            self.controls_widget.tool2.setStyleSheet('''
+                QPushButton {
+                    background-color: #989898; 
+                    color: black;
+                }
+                ''')
+            self.controls_widget.tool2.repaint()
+
+        elif event.key() == Qt.Key_8:
+            print("Tool 3")
+            self.controls_widget.tool3.setDown(True)
+            self.controls_widget.tool3.setStyleSheet('''
+                QPushButton {
+                    background-color: #989898; 
+                    color: black;
+                }
+                ''')
+            self.controls_widget.tool3.repaint()
+
+        elif event.key() == Qt.Key_9:
+            print("Tool 4")
+            self.controls_widget.tool4.setDown(True)
+            self.controls_widget.tool4.setStyleSheet('''
+                QPushButton {
+                    background-color: #989898; 
+                    color: black;
+                }
+                ''')
+            self.controls_widget.tool4.repaint()
+    
+    def keyReleaseEvent(self, event):
+
+        # checking for directional pitch key release events
+        if event.key() == Qt.Key_W:
+            self.controls_widget.tilt_forward.setDown(False)
+            self.controls_widget.tilt_forward.setStyleSheet('''
+                QPushButton {
+                    background-color: #BFBFBF; 
+                    color: black;
+                }
+                ''')
+            self.controls_widget.tilt_forward.repaint()
+        elif event.key() == Qt.Key_A:
+            self.controls_widget.tilt_left.setDown(False)
+            self.controls_widget.tilt_left.setStyleSheet('''
+                QPushButton {
+                    background-color: #BFBFBF; 
+                    color: black;
+                }
+                ''')
+            self.controls_widget.tilt_left.repaint()
+        elif event.key() == Qt.Key_S:
+            self.controls_widget.tilt_backwards.setDown(False)
+            self.controls_widget.tilt_backwards.setStyleSheet('''
+                QPushButton {
+                    background-color: #BFBFBF; 
+                    color: black;
+                }''')
+            self.controls_widget.tilt_backwards.repaint()
+        elif event.key() == Qt.Key_D:
+            self.controls_widget.tilt_right.setDown(False)
+            self.controls_widget.tilt_right.setStyleSheet('''
+                QPushButton {
+                    background-color: #BFBFBF;
+                    color: black;
+                }
+                ''')
+            self.controls_widget.tilt_right.repaint()
+
+        # checking for directional yaw key release events
+        elif event.key() == Qt.Key_Q:
+            self.controls_widget.yaw_left.setDown(False)
+            self.controls_widget.yaw_left.setStyleSheet('''
+                QPushButton {
+                    background-color: #BFBFBF; 
+                    color: black;
+                }
+                ''')
+            self.controls_widget.yaw_left.repaint()
+        elif event.key() == Qt.Key_E:
+            self.controls_widget.yaw_right.setDown(False)
+            self.controls_widget.yaw_right.setStyleSheet('''
+                QPushButton {
+                    background-color: #BFBFBF; 
+                    color: black;
+                }
+                ''')
+            self.controls_widget.yaw_right.repaint()
+
+        # Checking for ascent and descent key release events
+        elif event.key() == Qt.Key_R:
+            self.controls_widget.ascend.setDown(False)
+            self.controls_widget.ascend.setStyleSheet('''
+                QPushButton {
+                    background-color: #BFBFBF; 
+                    color: black;
+                }
+                ''')
+            self.controls_widget.ascend.repaint()
+        elif event.key() == Qt.Key_F:
+            self.controls_widget.descend.setDown(False)
+            self.controls_widget.descend.setStyleSheet('''
+                QPushButton {
+                    background-color: #BFBFBF; 
+                    color: black;
+                }
+                ''')
+            self.controls_widget.descend.repaint()
+
+        # checking for tool key release events
+        elif event.key() == Qt.Key_6:
+            self.controls_widget.tool1.setDown(False)
+            self.controls_widget.tool1.setStyleSheet('''
+                QPushButton {
+                    background-color: #BFBFBF; 
+                    color: black;
+                }
+                ''')
+            self.controls_widget.tool1.repaint()
+
+        elif event.key() == Qt.Key_7:
+            self.controls_widget.tool2.setDown(False)
+            self.controls_widget.tool2.setStyleSheet('''
+                QPushButton {
+                    background-color: #BFBFBF; 
+                    color: black;
+                }
+                ''')
+            self.controls_widget.tool2.repaint()
+
+        elif event.key() == Qt.Key_8:
+            self.controls_widget.tool3.setDown(False)
+            self.controls_widget.tool3.setStyleSheet('''
+                QPushButton {
+                    background-color: #BFBFBF; 
+                    color: black;
+                }
+                ''')
+            self.controls_widget.tool3.repaint()
+
+        elif event.key() == Qt.Key_9:
+            self.controls_widget.tool4.setDown(False)
+            self.controls_widget.tool4.setStyleSheet('''
+                QPushButton {
+                    background-color: #BFBFBF; 
+                    color: black;
+                }
+                ''')
+            self.controls_widget.tool4.repaint()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv) 
+    window = MainWindow()
+    window.show() 
+    app.exec()
---
+++ ui_sub/src/camera_stream.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# This is the stream widget class containing the four streams from the ROV -- 
+# three front facing cameras and one bottom facing camera. 
+# if you have a Mac, you will need to use the install command 'brew install gstreamer'
+# this will allow you to run gst-launch-1.0
+
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+import subprocess
+import cv2
+import numpy as np
+import command as command
+
+class CameraStreamWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initStream()
+
+    def initStream(self):
+
+        # creating the stream widget
+        self.stream = QLabel()
+        stream_layout = QGridLayout(self.stream)
+        self.setLayout(stream_layout)
+
+        # creating stream 1 window
+        self.stream1 = QLabel("Stream 1")
+        self.stream1.setAlignment(Qt.AlignCenter)
+        stream_layout.addWidget(self.stream1, 0, 0)
+
+        # creating stream 2 window
+        self.stream2 = QLabel("Stream 2")
+        self.stream2.setAlignment(Qt.AlignCenter)
+        stream_layout.addWidget(self.stream2, 0, 1)
+
+        # creating stream 3 window
+        self.stream3 = QLabel("Stream 3")
+        self.stream3.setAlignment(Qt.AlignCenter)
+        stream_layout.addWidget(self.stream3, 1, 0)
+
+        # creating stream 4 window
+        self.stream4 = QLabel("Stream 4")
+        self.stream4.setAlignment(Qt.AlignCenter)
+        stream_layout.addWidget(self.stream4, 1, 1)
+
+        # launching the stream 
+        self.launch_stream()
+
+    def launch_stream(self):
+
+        # put address from local machine into host command
+
+        # gstreamer commands for starting stream on the pi
+        launch_stream1 = "gst-launch-1.0 -v v4l2src device=/dev/video8 ! video/x-h264, width=1920,height=1080! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=10.0.0.101 port=5600 sync=false buffer-size=1048576"
+        launch_stream2 = "gst-launch-1.0 -v v4l2src device=/dev/video4 ! video/x-h264, width=1920,height=1080! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=10.0.0.101 port=5601 sync=false buffer-size=1048576"
+        launch_stream3 = "gst-launch-1.0 -v v4l2src device=/dev/video0 ! image/jpeg, width=1920,height=1080, framerate=30/1 ! jpegparse ! queue max-size-buffers=100 ! rtpjpegpay ! udpsink host=10.0.0.101 port=5602 sync=false buffer-size=1048576"
+
+        # starting subprocesses for each stream
+        # subprocess.Popen(launch_stream1, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1, universal_newlines=True)
+        # subprocess.run(launch_stream2, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1, universal_newlines=True)
+        # subprocess.run(launch_stream3, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1, universal_newlines=True)
+
+        # gstreamer pipeline for receiving stream 
+        receive_stream1 = "gst-launch-1.0 udpsrc port=5600 ! application/x-rtp ! rtpjitterbuffer ! rtph264depay ! avdec_h264 ! videoconvert ! autovideosink sync=false"
+        receive_stream2 = "gst-launch-1.0 udpsrc port=5601 ! application/x-rtp ! rtpjitterbuffer ! rtph264depay ! avdec_h264 ! videoconvert ! autovideosink sync=false"
+        # receive_stream3 = "gst-launch-1.0 -v v4l2src device=/dev/video0 ! image/jpeg, width=1920,height=1080, framerate=30/1 ! jpegparse ! queue max-size-buffers=100 ! rtpjpegpay ! udpsink host=10.0.0.101 port=5602 sync=false buffer-size=1048576"
+        
+        # starting subprocesses for each stream
+        self.stream1_process = subprocess.Popen(receive_stream1, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        self.stream2_process = subprocess.Popen(receive_stream2, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # self.stream3_process = subprocess.Popen(receive_stream3, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
