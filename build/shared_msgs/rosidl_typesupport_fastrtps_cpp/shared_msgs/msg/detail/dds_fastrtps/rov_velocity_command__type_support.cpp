9f75b67aacb7b9e6a03aec7fda1429b710aac0dc
Ben Boardley <bboard1202@gmail.com>
2023-10-25 14:49:45 -0400

    some changes

---
+++ build/shared_msgs/rosidl_typesupport_fastrtps_cpp/shared_msgs/msg/detail/dds_fastrtps/rov_velocity_command__type_support.cpp
@@ -0,0 +1,330 @@
+// generated from rosidl_typesupport_fastrtps_cpp/resource/idl__type_support.cpp.em
+// with input from shared_msgs:msg/RovVelocityCommand.idl
+// generated code does not contain a copyright notice
+#include "shared_msgs/msg/detail/rov_velocity_command__rosidl_typesupport_fastrtps_cpp.hpp"
+#include "shared_msgs/msg/detail/rov_velocity_command__struct.hpp"
+
+#include <limits>
+#include <stdexcept>
+#include <string>
+#include "rosidl_typesupport_cpp/message_type_support.hpp"
+#include "rosidl_typesupport_fastrtps_cpp/identifier.hpp"
+#include "rosidl_typesupport_fastrtps_cpp/message_type_support.h"
+#include "rosidl_typesupport_fastrtps_cpp/message_type_support_decl.hpp"
+#include "rosidl_typesupport_fastrtps_cpp/wstring_conversion.hpp"
+#include "fastcdr/Cdr.h"
+
+
+// forward declaration of message dependencies and their conversion functions
+namespace geometry_msgs
+{
+namespace msg
+{
+namespace typesupport_fastrtps_cpp
+{
+bool cdr_serialize(
+  const geometry_msgs::msg::Twist &,
+  eprosima::fastcdr::Cdr &);
+bool cdr_deserialize(
+  eprosima::fastcdr::Cdr &,
+  geometry_msgs::msg::Twist &);
+size_t get_serialized_size(
+  const geometry_msgs::msg::Twist &,
+  size_t current_alignment);
+size_t
+max_serialized_size_Twist(
+  bool & full_bounded,
+  bool & is_plain,
+  size_t current_alignment);
+}  // namespace typesupport_fastrtps_cpp
+}  // namespace msg
+}  // namespace geometry_msgs
+
+
+namespace shared_msgs
+{
+
+namespace msg
+{
+
+namespace typesupport_fastrtps_cpp
+{
+
+bool
+ROSIDL_TYPESUPPORT_FASTRTPS_CPP_PUBLIC_shared_msgs
+cdr_serialize(
+  const shared_msgs::msg::RovVelocityCommand & ros_message,
+  eprosima::fastcdr::Cdr & cdr)
+{
+  // Member: twist
+  geometry_msgs::msg::typesupport_fastrtps_cpp::cdr_serialize(
+    ros_message.twist,
+    cdr);
+  // Member: source
+  cdr << ros_message.source;
+  // Member: is_fine
+  cdr << ros_message.is_fine;
+  // Member: multiplier
+  cdr << ros_message.multiplier;
+  // Member: is_percent_power
+  cdr << (ros_message.is_percent_power ? true : false);
+  // Member: is_pool_centric
+  cdr << (ros_message.is_pool_centric ? true : false);
+  return true;
+}
+
+bool
+ROSIDL_TYPESUPPORT_FASTRTPS_CPP_PUBLIC_shared_msgs
+cdr_deserialize(
+  eprosima::fastcdr::Cdr & cdr,
+  shared_msgs::msg::RovVelocityCommand & ros_message)
+{
+  // Member: twist
+  geometry_msgs::msg::typesupport_fastrtps_cpp::cdr_deserialize(
+    cdr, ros_message.twist);
+
+  // Member: source
+  cdr >> ros_message.source;
+
+  // Member: is_fine
+  cdr >> ros_message.is_fine;
+
+  // Member: multiplier
+  cdr >> ros_message.multiplier;
+
+  // Member: is_percent_power
+  {
+    uint8_t tmp;
+    cdr >> tmp;
+    ros_message.is_percent_power = tmp ? true : false;
+  }
+
+  // Member: is_pool_centric
+  {
+    uint8_t tmp;
+    cdr >> tmp;
+    ros_message.is_pool_centric = tmp ? true : false;
+  }
+
+  return true;
+}
+
+size_t
+ROSIDL_TYPESUPPORT_FASTRTPS_CPP_PUBLIC_shared_msgs
+get_serialized_size(
+  const shared_msgs::msg::RovVelocityCommand & ros_message,
+  size_t current_alignment)
+{
+  size_t initial_alignment = current_alignment;
+
+  const size_t padding = 4;
+  const size_t wchar_size = 4;
+  (void)padding;
+  (void)wchar_size;
+
+  // Member: twist
+
+  current_alignment +=
+    geometry_msgs::msg::typesupport_fastrtps_cpp::get_serialized_size(
+    ros_message.twist, current_alignment);
+  // Member: source
+  current_alignment += padding +
+    eprosima::fastcdr::Cdr::alignment(current_alignment, padding) +
+    (ros_message.source.size() + 1);
+  // Member: is_fine
+  {
+    size_t item_size = sizeof(ros_message.is_fine);
+    current_alignment += item_size +
+      eprosima::fastcdr::Cdr::alignment(current_alignment, item_size);
+  }
+  // Member: multiplier
+  {
+    size_t item_size = sizeof(ros_message.multiplier);
+    current_alignment += item_size +
+      eprosima::fastcdr::Cdr::alignment(current_alignment, item_size);
+  }
+  // Member: is_percent_power
+  {
+    size_t item_size = sizeof(ros_message.is_percent_power);
+    current_alignment += item_size +
+      eprosima::fastcdr::Cdr::alignment(current_alignment, item_size);
+  }
+  // Member: is_pool_centric
+  {
+    size_t item_size = sizeof(ros_message.is_pool_centric);
+    current_alignment += item_size +
+      eprosima::fastcdr::Cdr::alignment(current_alignment, item_size);
+  }
+
+  return current_alignment - initial_alignment;
+}
+
+size_t
+ROSIDL_TYPESUPPORT_FASTRTPS_CPP_PUBLIC_shared_msgs
+max_serialized_size_RovVelocityCommand(
+  bool & full_bounded,
+  bool & is_plain,
+  size_t current_alignment)
+{
+  size_t initial_alignment = current_alignment;
+
+  const size_t padding = 4;
+  const size_t wchar_size = 4;
+  (void)padding;
+  (void)wchar_size;
+
+  full_bounded = true;
+  is_plain = true;
+
+
+  // Member: twist
+  {
+    size_t array_size = 1;
+
+
+    for (size_t index = 0; index < array_size; ++index) {
+      bool inner_full_bounded;
+      bool inner_is_plain;
+      current_alignment +=
+        geometry_msgs::msg::typesupport_fastrtps_cpp::max_serialized_size_Twist(
+        inner_full_bounded, inner_is_plain, current_alignment);
+      full_bounded &= inner_full_bounded;
+      is_plain &= inner_is_plain;
+    }
+  }
+
+  // Member: source
+  {
+    size_t array_size = 1;
+
+    full_bounded = false;
+    is_plain = false;
+    for (size_t index = 0; index < array_size; ++index) {
+      current_alignment += padding +
+        eprosima::fastcdr::Cdr::alignment(current_alignment, padding) +
+        1;
+    }
+  }
+
+  // Member: is_fine
+  {
+    size_t array_size = 1;
+
+    current_alignment += array_size * sizeof(uint8_t);
+  }
+
+  // Member: multiplier
+  {
+    size_t array_size = 1;
+
+    current_alignment += array_size * sizeof(uint32_t) +
+      eprosima::fastcdr::Cdr::alignment(current_alignment, sizeof(uint32_t));
+  }
+
+  // Member: is_percent_power
+  {
+    size_t array_size = 1;
+
+    current_alignment += array_size * sizeof(uint8_t);
+  }
+
+  // Member: is_pool_centric
+  {
+    size_t array_size = 1;
+
+    current_alignment += array_size * sizeof(uint8_t);
+  }
+
+  return current_alignment - initial_alignment;
+}
+
+static bool _RovVelocityCommand__cdr_serialize(
+  const void * untyped_ros_message,
+  eprosima::fastcdr::Cdr & cdr)
+{
+  auto typed_message =
+    static_cast<const shared_msgs::msg::RovVelocityCommand *>(
+    untyped_ros_message);
+  return cdr_serialize(*typed_message, cdr);
+}
+
+static bool _RovVelocityCommand__cdr_deserialize(
+  eprosima::fastcdr::Cdr & cdr,
+  void * untyped_ros_message)
+{
+  auto typed_message =
+    static_cast<shared_msgs::msg::RovVelocityCommand *>(
+    untyped_ros_message);
+  return cdr_deserialize(cdr, *typed_message);
+}
+
+static uint32_t _RovVelocityCommand__get_serialized_size(
+  const void * untyped_ros_message)
+{
+  auto typed_message =
+    static_cast<const shared_msgs::msg::RovVelocityCommand *>(
+    untyped_ros_message);
+  return static_cast<uint32_t>(get_serialized_size(*typed_message, 0));
+}
+
+static size_t _RovVelocityCommand__max_serialized_size(char & bounds_info)
+{
+  bool full_bounded;
+  bool is_plain;
+  size_t ret_val;
+
+  ret_val = max_serialized_size_RovVelocityCommand(full_bounded, is_plain, 0);
+
+  bounds_info =
+    is_plain ? ROSIDL_TYPESUPPORT_FASTRTPS_PLAIN_TYPE :
+    full_bounded ? ROSIDL_TYPESUPPORT_FASTRTPS_BOUNDED_TYPE : ROSIDL_TYPESUPPORT_FASTRTPS_UNBOUNDED_TYPE;
+  return ret_val;
+}
+
+static message_type_support_callbacks_t _RovVelocityCommand__callbacks = {
+  "shared_msgs::msg",
+  "RovVelocityCommand",
+  _RovVelocityCommand__cdr_serialize,
+  _RovVelocityCommand__cdr_deserialize,
+  _RovVelocityCommand__get_serialized_size,
+  _RovVelocityCommand__max_serialized_size
+};
+
+static rosidl_message_type_support_t _RovVelocityCommand__handle = {
+  rosidl_typesupport_fastrtps_cpp::typesupport_identifier,
+  &_RovVelocityCommand__callbacks,
+  get_message_typesupport_handle_function,
+};
+
+}  // namespace typesupport_fastrtps_cpp
+
+}  // namespace msg
+
+}  // namespace shared_msgs
+
+namespace rosidl_typesupport_fastrtps_cpp
+{
+
+template<>
+ROSIDL_TYPESUPPORT_FASTRTPS_CPP_EXPORT_shared_msgs
+const rosidl_message_type_support_t *
+get_message_type_support_handle<shared_msgs::msg::RovVelocityCommand>()
+{
+  return &shared_msgs::msg::typesupport_fastrtps_cpp::_RovVelocityCommand__handle;
+}
+
+}  // namespace rosidl_typesupport_fastrtps_cpp
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+const rosidl_message_type_support_t *
+ROSIDL_TYPESUPPORT_INTERFACE__MESSAGE_SYMBOL_NAME(rosidl_typesupport_fastrtps_cpp, shared_msgs, msg, RovVelocityCommand)() {
+  return &shared_msgs::msg::typesupport_fastrtps_cpp::_RovVelocityCommand__handle;
+}
+
+#ifdef __cplusplus
+}
+#endif
---
+++ build/shared_msgs/rosidl_typesupport_introspection_c/shared_msgs/msg/detail/rov_velocity_command__type_support.c
@@ -0,0 +1,178 @@
+// generated from rosidl_typesupport_introspection_c/resource/idl__type_support.c.em
+// with input from shared_msgs:msg/RovVelocityCommand.idl
+// generated code does not contain a copyright notice
+
+#include <stddef.h>
+#include "shared_msgs/msg/detail/rov_velocity_command__rosidl_typesupport_introspection_c.h"
+#include "shared_msgs/msg/rosidl_typesupport_introspection_c__visibility_control.h"
+#include "rosidl_typesupport_introspection_c/field_types.h"
+#include "rosidl_typesupport_introspection_c/identifier.h"
+#include "rosidl_typesupport_introspection_c/message_introspection.h"
+#include "shared_msgs/msg/detail/rov_velocity_command__functions.h"
+#include "shared_msgs/msg/detail/rov_velocity_command__struct.h"
+
+
+// Include directives for member types
+// Member `twist`
+#include "geometry_msgs/msg/twist.h"
+// Member `twist`
+#include "geometry_msgs/msg/detail/twist__rosidl_typesupport_introspection_c.h"
+// Member `source`
+#include "rosidl_runtime_c/string_functions.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+void shared_msgs__msg__RovVelocityCommand__rosidl_typesupport_introspection_c__RovVelocityCommand_init_function(
+  void * message_memory, enum rosidl_runtime_c__message_initialization _init)
+{
+  // TODO(karsten1987): initializers are not yet implemented for typesupport c
+  // see https://github.com/ros2/ros2/issues/397
+  (void) _init;
+  shared_msgs__msg__RovVelocityCommand__init(message_memory);
+}
+
+void shared_msgs__msg__RovVelocityCommand__rosidl_typesupport_introspection_c__RovVelocityCommand_fini_function(void * message_memory)
+{
+  shared_msgs__msg__RovVelocityCommand__fini(message_memory);
+}
+
+static rosidl_typesupport_introspection_c__MessageMember shared_msgs__msg__RovVelocityCommand__rosidl_typesupport_introspection_c__RovVelocityCommand_message_member_array[6] = {
+  {
+    "twist",  // name
+    rosidl_typesupport_introspection_c__ROS_TYPE_MESSAGE,  // type
+    0,  // upper bound of string
+    NULL,  // members of sub message (initialized later)
+    false,  // is array
+    0,  // array size
+    false,  // is upper bound
+    offsetof(shared_msgs__msg__RovVelocityCommand, twist),  // bytes offset in struct
+    NULL,  // default value
+    NULL,  // size() function pointer
+    NULL,  // get_const(index) function pointer
+    NULL,  // get(index) function pointer
+    NULL,  // fetch(index, &value) function pointer
+    NULL,  // assign(index, value) function pointer
+    NULL  // resize(index) function pointer
+  },
+  {
+    "source",  // name
+    rosidl_typesupport_introspection_c__ROS_TYPE_STRING,  // type
+    0,  // upper bound of string
+    NULL,  // members of sub message
+    false,  // is array
+    0,  // array size
+    false,  // is upper bound
+    offsetof(shared_msgs__msg__RovVelocityCommand, source),  // bytes offset in struct
+    NULL,  // default value
+    NULL,  // size() function pointer
+    NULL,  // get_const(index) function pointer
+    NULL,  // get(index) function pointer
+    NULL,  // fetch(index, &value) function pointer
+    NULL,  // assign(index, value) function pointer
+    NULL  // resize(index) function pointer
+  },
+  {
+    "is_fine",  // name
+    rosidl_typesupport_introspection_c__ROS_TYPE_UINT8,  // type
+    0,  // upper bound of string
+    NULL,  // members of sub message
+    false,  // is array
+    0,  // array size
+    false,  // is upper bound
+    offsetof(shared_msgs__msg__RovVelocityCommand, is_fine),  // bytes offset in struct
+    NULL,  // default value
+    NULL,  // size() function pointer
+    NULL,  // get_const(index) function pointer
+    NULL,  // get(index) function pointer
+    NULL,  // fetch(index, &value) function pointer
+    NULL,  // assign(index, value) function pointer
+    NULL  // resize(index) function pointer
+  },
+  {
+    "multiplier",  // name
+    rosidl_typesupport_introspection_c__ROS_TYPE_FLOAT,  // type
+    0,  // upper bound of string
+    NULL,  // members of sub message
+    false,  // is array
+    0,  // array size
+    false,  // is upper bound
+    offsetof(shared_msgs__msg__RovVelocityCommand, multiplier),  // bytes offset in struct
+    NULL,  // default value
+    NULL,  // size() function pointer
+    NULL,  // get_const(index) function pointer
+    NULL,  // get(index) function pointer
+    NULL,  // fetch(index, &value) function pointer
+    NULL,  // assign(index, value) function pointer
+    NULL  // resize(index) function pointer
+  },
+  {
+    "is_percent_power",  // name
+    rosidl_typesupport_introspection_c__ROS_TYPE_BOOLEAN,  // type
+    0,  // upper bound of string
+    NULL,  // members of sub message
+    false,  // is array
+    0,  // array size
+    false,  // is upper bound
+    offsetof(shared_msgs__msg__RovVelocityCommand, is_percent_power),  // bytes offset in struct
+    NULL,  // default value
+    NULL,  // size() function pointer
+    NULL,  // get_const(index) function pointer
+    NULL,  // get(index) function pointer
+    NULL,  // fetch(index, &value) function pointer
+    NULL,  // assign(index, value) function pointer
+    NULL  // resize(index) function pointer
+  },
+  {
+    "is_pool_centric",  // name
+    rosidl_typesupport_introspection_c__ROS_TYPE_BOOLEAN,  // type
+    0,  // upper bound of string
+    NULL,  // members of sub message
+    false,  // is array
+    0,  // array size
+    false,  // is upper bound
+    offsetof(shared_msgs__msg__RovVelocityCommand, is_pool_centric),  // bytes offset in struct
+    NULL,  // default value
+    NULL,  // size() function pointer
+    NULL,  // get_const(index) function pointer
+    NULL,  // get(index) function pointer
+    NULL,  // fetch(index, &value) function pointer
+    NULL,  // assign(index, value) function pointer
+    NULL  // resize(index) function pointer
+  }
+};
+
+static const rosidl_typesupport_introspection_c__MessageMembers shared_msgs__msg__RovVelocityCommand__rosidl_typesupport_introspection_c__RovVelocityCommand_message_members = {
+  "shared_msgs__msg",  // message namespace
+  "RovVelocityCommand",  // message name
+  6,  // number of fields
+  sizeof(shared_msgs__msg__RovVelocityCommand),
+  shared_msgs__msg__RovVelocityCommand__rosidl_typesupport_introspection_c__RovVelocityCommand_message_member_array,  // message members
+  shared_msgs__msg__RovVelocityCommand__rosidl_typesupport_introspection_c__RovVelocityCommand_init_function,  // function to initialize message memory (memory has to be allocated)
+  shared_msgs__msg__RovVelocityCommand__rosidl_typesupport_introspection_c__RovVelocityCommand_fini_function  // function to terminate message instance (will not free memory)
+};
+
+// this is not const since it must be initialized on first access
+// since C does not allow non-integral compile-time constants
+static rosidl_message_type_support_t shared_msgs__msg__RovVelocityCommand__rosidl_typesupport_introspection_c__RovVelocityCommand_message_type_support_handle = {
+  0,
+  &shared_msgs__msg__RovVelocityCommand__rosidl_typesupport_introspection_c__RovVelocityCommand_message_members,
+  get_message_typesupport_handle_function,
+};
+
+ROSIDL_TYPESUPPORT_INTROSPECTION_C_EXPORT_shared_msgs
+const rosidl_message_type_support_t *
+ROSIDL_TYPESUPPORT_INTERFACE__MESSAGE_SYMBOL_NAME(rosidl_typesupport_introspection_c, shared_msgs, msg, RovVelocityCommand)() {
+  shared_msgs__msg__RovVelocityCommand__rosidl_typesupport_introspection_c__RovVelocityCommand_message_member_array[0].members_ =
+    ROSIDL_TYPESUPPORT_INTERFACE__MESSAGE_SYMBOL_NAME(rosidl_typesupport_introspection_c, geometry_msgs, msg, Twist)();
+  if (!shared_msgs__msg__RovVelocityCommand__rosidl_typesupport_introspection_c__RovVelocityCommand_message_type_support_handle.typesupport_identifier) {
+    shared_msgs__msg__RovVelocityCommand__rosidl_typesupport_introspection_c__RovVelocityCommand_message_type_support_handle.typesupport_identifier =
+      rosidl_typesupport_introspection_c__identifier;
+  }
+  return &shared_msgs__msg__RovVelocityCommand__rosidl_typesupport_introspection_c__RovVelocityCommand_message_type_support_handle;
+}
+#ifdef __cplusplus
+}
+#endif
