4e27c9befb0046a276b7947db8151f5675f8d28d
Adam Kahl <adamkahl8@gmail.com>
2024-01-20 16:14:40 -0500

    Got gamepad working with pygame

--- ros/gamepad/src/config.py
+++ ros/gamepad/src/config.py
@@ -1,56 +1,26 @@
 MIN_ABS_DIFFERENCE = 0
-# Old ranges for the triggers and sticks
-# TRIGGER_DEAD_ZONE = 50
-# TRIGGER_RANGE = 1025
-# STICK_DEAD_ZONE = 4000
-# STICK_RANGE = 32768
 # New ranges for the triggers and sticks (Probably need to adjust these values)
 TRIGGER_DEAD_ZONE = 0.09
 TRIGGER_RANGE = 1.0
 STICK_DEAD_ZONE = 0.09
 STICK_RANGE = 1.0
 
-EVENT_MISC = "Misc"
-EVENT_SYNC = "Sync"
-EVENT_ABSOLUTE = "Absolute"
-EVENT_KEY = "Key"
+# Mapping of JoyAxisMotion events to gamepad_state keys
+# The number is the axis given by pygame
+JOY_AXIS = {1: 'LSY', 0: 'LSX', 4: 'RSY', 3: 'RSX', 2: 'LT', 5: 'RT'}
 
-ignore_events = [EVENT_MISC, EVENT_SYNC]
-
-EVENTS = {
-    # Joysticks
-    "ABS_X": "LSX",
-    "ABS_Y": "LSY",
-    "ABS_RX": "RSX",
-    "ABS_RY": "RSY",
-    # Triggers
-    "ABS_Z": "LT",
-    "ABS_RZ": "RT",
-    # DPad
-    "ABS_HAT0X": "DPADX",
-    "ABS_HAT0Y": "DPADY",
-    # Face Buttons
-    "BTN_NORTH": "Y",
-    "BTN_EAST": "B",
-    "BTN_SOUTH": "A",
-    "BTN_WEST": "X",
-    # Other buttons
-    "BTN_THUMBL": "LSZ",
-    "BTN_THUMBR": "RSZ",
-    "BTN_TL": "LB",
-    "BTN_TR": "RB",
-    "BTN_MODE": "XBOX",
-    "BTN_START": "START",
-    "BTN_SELECT": "MENU",
-}
+# Mapping of JoyButton events to gamepad_state keys
+# The number is the button given by pygame
+JOY_BUTTON = {3: 'Y', 1: 'B', 0: 'A', 2: 'X', 9: 'LSZ', 10: 'RSZ', 4: 'LB', 5: 'RB', 8: 'XBOX', 6: 'START', 7: 'MENU'}
+JOY_BUTTON_KEY = {'Y': 3, 'B': 1, 'A': 0, 'X': 2, 'LSZ': 9, 'RSZ': 10, 'LB': 4, 'RB': 5, 'XBOX': 8, 'START': 6, 'MENU': 7}
 
 gamepad_state = {
     "LSX": 0.0,
     "LSY": 0.0,
     "RSX": 0.0,
     "RSY": 0.0,
-    "LT": 0.0,
-    "RT": 0.0,
+    "LT": -1.0,
+    "RT": -1.0,
     "DPADX": 0,
     "DPADY": 0,
     "Y": 0,
--- ros/gamepad/src/sender.py
+++ ros/gamepad/src/sender.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-from inputs import get_gamepad
 import pygame
 import sys
 
@@ -12,14 +11,6 @@ from geometry_msgs.msg import Twist
 
 from config import *
 
-# Mapping of JoyAxisMotion events to gamepad_state keys
-# The number is the axis given by pygame
-JOY_AXIS = {1: 'LSY', 0: 'LSX', 4: 'RSY', 3: 'RSX', 2: 'LT', 5: 'RT'}
-
-# Mapping of JoyButton events to gamepad_state keys
-# The number is the button given by pygame
-JOY_BUTTON = {3: 'Y', 1: 'B', 0: 'A', 2: 'X', 9: 'LSZ', 10: 'RSZ', 4: 'LB', 5: 'RB', 8: 'XBOX', 6: 'START', 7: 'MENU'}
-JOY_BUTTON_KEY = {'Y': 3, 'B': 1, 'A': 0, 'X': 2, 'LSZ': 9, 'RSZ': 10, 'LB': 4, 'RB': 5, 'XBOX': 8, 'START': 6, 'MENU': 7}
 
 tools = [0, 0, 0, 0, 0]
 
@@ -46,7 +37,9 @@ depth_lock = False
 pitch_lock = False
 GAMEPAD_TIMEOUT = 20 # seconds
 
+
 def getMessage():
+    '''Returns a RovVelocityCommand message based on the current gamepad state'''
     global gamepad_state
     global is_fine
     global is_pool_centric
@@ -57,7 +50,7 @@ def getMessage():
 
     t.linear.x = -(gamepad_state['LSY'] * SCALE_TRANSLATIONAL_X + TRIM_X) * REVERSE
     t.linear.y = -(gamepad_state['LSX'] * SCALE_TRANSLATIONAL_Y + TRIM_Y) * REVERSE
-    t.linear.z = (gamepad_state['RT'] - gamepad_state['LT']) * SCALE_TRANSLATIONAL_Z + TRIM_Z
+    t.linear.z = ((gamepad_state['RT'] - gamepad_state['LT']) / 2.0) * SCALE_TRANSLATIONAL_Z + TRIM_Z
 
     if gamepad_state['LB'] == 1:
         x = 1 * SCALE_ROTATIONAL_X
@@ -79,7 +72,9 @@ def getMessage():
 
     return(new_msg)
 
+
 def getTools():
+    '''Returns a ToolsCommandMsg message based on the current gamepad state'''
     global tools
 
     tm = ToolsCommandMsg()
@@ -87,19 +82,14 @@ def getTools():
 
     return tm
 
+
 def correct_raw(raw, abbv):
+    '''Corrects the raw value from the gamepad to be in the range [-1.0, 1.0]'''
     # Separate the sign from the value
     sign = (raw >= 0) * 2 - 1
     raw = abs(raw)
-    # Old code to check if the input is a trigger or a stick
-    # if abbv[1] == 'T':
-    #     dead_zone = TRIGGER_DEAD_ZONE
-    #     value_range = TRIGGER_RANGE
-    # else:
-    #     dead_zone = STICK_DEAD_ZONE
-    #     value_range = STICK_RANGE
 
-    # New code to check if the input is a trigger or a stick
+    # Check if the input is a trigger or a stick
     if abbv == 'LT' or abbv == 'RT':
         dead_zone = TRIGGER_DEAD_ZONE
         value_range = TRIGGER_RANGE
@@ -118,37 +108,17 @@ def correct_raw(raw, abbv):
     corrected *= sign
     return corrected
 
+
 def process_event(event):
+    '''Processes a pygame event'''
     global tools
     global is_fine
     global gamepad_state
     global is_pool_centric
     global depth_lock
     global pitch_lock
-    # if event.ev_type in ignore_events:
-    #     return
 
-    # A, B, X, and Y buttons
-    # OLD CODE USING INPUTS LIBRARY
-    # if event.ev_type == EVENT_KEY:
-    #     gamepad_state[EVENTS[event.code]] = event.state
-    #     if event.code == 'BTN_SOUTH' and event.state:
-    #         tools[0] = not tools[0]
-    #
-    #     if event.code == 'BTN_EAST' and event.state:
-    #         tools[1] = not tools[1]
-    #
-    #     if event.code == 'BTN_WEST' and event.state:
-    #         tools[2] = not tools[2]
-    #
-    #     if event.code == 'BTN_NORTH' and event.state and LOCKOUT:
-    #         tools[3] = not tools[3]
-    #
-    #     if event.code == 'BTN_SELECT' and event.state:
-    #         is_pool_centric = not is_pool_centric
-
-    # All the buttons
-    # NEW CODE USING PYGAME LIBRARY (Includes bumpers, joystick buttons, view, and start)
+    # Button pressed down events
     if event.type == pygame.JOYBUTTONDOWN:
         gamepad_state[JOY_BUTTON[event.button]] = 1
         if event.button == JOY_BUTTON_KEY['A']:
@@ -166,32 +136,12 @@ def process_event(event):
         elif event.button == JOY_BUTTON_KEY['MENU']:
             is_pool_centric = not is_pool_centric
         
-        
 
+    # Button released events    
     elif event.type == pygame.JOYBUTTONUP:
         gamepad_state[JOY_BUTTON[event.button]] = 0
 
     # DPAD buttons
-    # OLD CODE USING INPUTS LIBRARY
-    # elif event.ev_type == EVENT_ABSOLUTE:
-    #     if event.code == 'ABS_HAT0Y' and event.state==-1:
-    #         if is_fine < 2:
-    #             is_fine +=1
-    #     elif event.code == 'ABS_HAT0Y' and event.state==1:
-    #         if is_fine > 0:
-    #             is_fine -=1
-    #     else:
-    #         pass
-    #     if event.code == 'ABS_HAT0X' and event.state==-1:
-    #         pitch_lock = not pitch_lock
-    #     elif event.code == 'ABS_HAT0X' and event.state==1:
-    #         depth_lock = not depth_lock
-    #         is_pool_centric = True
-    #     else:
-    #         pass
-
-    # DPAD buttons
-    # NEW CODE USING PYGAME LIBRARY
     elif event.type == pygame.JOYHATMOTION:
         if event.value[1] == -1:
             if is_fine < 2:
@@ -209,46 +159,45 @@ def process_event(event):
         else:
             pass
 
-    # Joysticks?
-    # OLD CODE USING INPUTS LIBRARY
-    #     gamepad_state[EVENTS[event.code]] = correct_raw(event.state, EVENTS[event.code])
-
     # Joysticks
-    # NEW CODE USING PYGAME LIBRARY
     elif event.type == pygame.JOYAXISMOTION:
         gamepad_state[JOY_AXIS[event.axis]] = correct_raw(event.value, JOY_AXIS[event.axis])
 
     
-    # If the gamepad is disconnected, reconnect it
+    # If the gamepad is disconnected, try to reconnect it
     elif event.type == pygame.JOYDEVICEREMOVED:
+        print('Gamepad disconnected')
         reconnect_gamepad()
-        
-
-    # Not sure what the other events might be
-    # else:
-    #     gamepad_state[EVENTS[event.code]] = event.state
 
 def pub_data():
+    '''Publishes the data to the rov_velocity topic and the tools topic'''
+    # Get a message to publish for the rov_velocity topic
     pub.publish(getMessage())
+    # Get a message to publish for the tools topic
     pub_tools.publish(getTools())
 
 def update_gamepad():
-    try:
-        event = pygame.event.poll()
+    '''Updates the gamepad state'''
+    # Get all the events from pygame and process them
+    for event in pygame.event.get():
         process_event(event)
-    except Exception:
-        pass
-        # logger = node.get_logger().info('no gamepad')
-        # rclpy.shutdown()
+
 
 def init_pygame():
+    '''Initializes pygame and the joystick'''
+    global joystick
     pygame.init()
     pygame.joystick.init()
     assert pygame.joystick.get_count() == 1
+    joystick = pygame.joystick.Joystick(0)
+
 
 def reconnect_gamepad():
+    '''Tries to reconnect the gamepad'''
+    global joystick
     for i in range(GAMEPAD_TIMEOUT):
         try:
+            print('Please reconnect the gamepad within {} seconds'.format(GAMEPAD_TIMEOUT - i))
             pygame.init()
             pygame.joystick.init()
             # make sure there is only one joystick
@@ -266,7 +215,7 @@ def reconnect_gamepad():
             # wait 1 second
             pygame.time.wait(1000)
     if not reconnected:
-        print('Gamepad not reconnected')
+        print('Gamepad not reconnected, exiting...')
         pygame.quit()
         rclpy.shutdown()
         sys.exit(0)
@@ -276,16 +225,18 @@ if __name__ == '__main__':
     global pub, pub_tools, data_thread, gamepad_thread
 
     try:
-       get_gamepad()
+        init_pygame()
     except:
-        sys.exit(0)
+        print('No gamepad found')
+        print('Please connect a gamepad')
+        reconnect_gamepad()
+    
 
     rclpy.init()
     node = rclpy.create_node('gp_pub')
 
     pub = node.create_publisher(RovVelocityCommand, 'rov_velocity', 10)
     pub_tools = node.create_publisher(ToolsCommandMsg, 'tools', 10)
-
 
     data_thread = node.create_timer(0.1, pub_data)
     gamepad_thread = node.create_timer(0.001, update_gamepad)
