82facf6b24b6a447f061e537c2cbb190b7cb7ecf
Ben Boardley <bboardle@purdue.edu>
2024-04-10 20:26:40 -0400

    deleted build folder

--- build/com_pub/CMakeFiles/com_pub_uninstall.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for com_pub_uninstall.
-# This may be replaced when dependencies are built.
--- build/ui/CMakeFiles/ament_cmake_python_build_ui_egg.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ament_cmake_python_build_ui_egg"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ament_cmake_python_build_ui_egg.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
