96db794c24d71fc1da193537158334e7fda435dd
firestrom0986 <firestorm0986@gmail.com>
2024-10-12 14:26:41 -0400

    Clean up code
    Co-authored-by: KiraYang3 <KiraYang3@users.noreply.github.com>

--- ui/src/ssh.py
+++ ui/src/ssh.py
@@ -16,11 +16,8 @@ class Ssh:
 
     def __init__(self, node):
         self.ssh_hostname = os.getenv("HOST_IP") # The IP address of the PI
-        print(self.ssh_hostname)
         self.ssh_password = os.getenv("HOST_PASSWORD") # The Password of the PI
-        print(self.ssh_password)
         self.ssh_username = os.getenv("HOST_USERNAME") # The username of the PI
-        print(self.ssh_username)
 
         self.ssh_client = None # Variable for the SSH client object
         self.connection = None # Variable for checking if connection is present
@@ -33,39 +30,23 @@ class Ssh:
             # getting the local ip address
             ip = self.get_ip()
             print(f"Local IP address: {ip}")
-            #ros_id = 69
-            # commands to launch on the pi
-            ros2_source_cmd = "source ~/.bashrc >> ~/ros2_ws/startup_logs/sourcebash.txt && export ROS_DOMAIN_ID=69 && source ros2_ws/install/setup.bash >> ~/ros2_ws/startup_logs/source.txt && echo $ROS_DOMAIN_ID >> ~/ros2_ws/startup_logs/domain_id_tmux"
-            #ros2_launch_cmd = "ros2 launch rov_launch run_rov_launch.xml >> ~/ros2_ws/startup_logs/launch.txt"
-            stream1_launch_cmd = f"gst-launch-1.0 -v v4l2src device={self.device_name1} ! video/x-h264, width=1920,height=1080! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host={ip} port=5600 sync=false buffer-size=1048576 & echo $! > pid.txt"
-            stream2_launch_cmd = f"gst-launch-1.0 -v v4l2src device={self.device_name2} ! video/x-h264, width=1920,height=1080! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host={ip} port=5601 sync=false buffer-size=1048576 & echo $! > pid.txt"
-            stream3_launch_cmd = f"gst-launch-1.0 -v v4l2src device={self.device_name3} ! video/x-h264, width=1920,height=1080! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host={ip} port=5602 sync=false buffer-size=1048576 & echo $! > pid.txt"
-            stream4_launch_cmd = f"gst-launch-1.0 -v v4l2src device={self.device_name4} ! video/x-h264, width=1920,height=1080! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host={ip} port=5603 sync=false buffer-size=1048576 & echo $! > pid.txt"
-
+            
             self.ssh_client = paramiko.SSHClient()
             # Add device to known hosts so it can connect.
-            # if not present gives error - AttributeError: 'NoneType' object has no attribute 'time'
             self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             # Connect to the host
             self.ssh_client.connect(self.ssh_hostname, username=self.ssh_username, password=self.ssh_password)
             if self.ssh_client is not None: 
                 self.node.get_logger().info("SSH Connection Established")
-
                 print("SSH Connection Established")
                 self.connection = True
-                self.ssh_client.exec_command("ls")
+                # self.ssh_client.exec_command("ls")
             else:
+                self.node.get_logger().info("SSH Connection Failed")
                 print("ERROR: SSH Connection Failed")
-
-            # launching the camera streams on the pi
-            self.launch_stream(1, stream1_launch_cmd)
-            self.launch_stream(2, stream2_launch_cmd)
-            self.launch_stream(3, stream3_launch_cmd)
-            self.launch_stream(4, stream4_launch_cmd)
-
             return self.connection
-                
         except Exception as e:
+            self.node.get_logger().info(f"ERROR: {e}")
             print(f"ERROR: {e}")
 
     def get_ip(self):
@@ -83,13 +64,6 @@ class Ssh:
 
     
     def close(self):
-        
-        # This is a program to stop ros2 and more
-
-        # if self.ssh_client is not None:
-        #     self.ssh_client.exec_command(
-        #         "ps aux | grep ros2 | awk '{print $2}' | xargs kill -9 && tmux kill-session -t ros2_session")
-
         # closing the ssh connection
         if self.ssh_client is not None:
             self.ssh_client.close()
--- ui/src/streams.py
+++
@@ -1,5 +0,0 @@
-## TODO: Automate Streams connections
-
-class Streams:
-    def __init__(self, connection):
-        
