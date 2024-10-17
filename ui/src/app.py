5f50ba58f2b2d86848e4ba8f84664cc138a473d6
ethanburmane <ethanburmane@gmail.com>
2024-10-17 22:40:21 +0000

    fixed problems, now using env in index.html

--- ui/src/app.py
+++ ui/src/app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", rov_ip=os.getenv("ROV_IP"))
 
 
 def initialize_frontend_nodes():
