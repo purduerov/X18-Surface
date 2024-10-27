29ed7bc4a5602e37cd9b6d84507dbec0dd5a7a5f
Icyadam14 <adamkahl8@gmail.com>
2024-10-27 17:07:56 -0400

    Added colcon to installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -25,7 +25,7 @@ fi
 echo "Installing ROS 2 - Humble"
 
 # Set the locale
-sudo apt update && sudo apt install locales
+sudo apt update && sudo apt install -y locales
 sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
@@ -33,11 +33,11 @@ export LANG=en_US.UTF-8
 # Setup sources
 
 # Ensure Ubuntu Universe repository is enabled
-sudo apt install software-properties-common
-sudo add-apt-repository universe
+sudo apt install -y software-properties-common
+sudo add-apt-repository -y universe
 
 # Add the ROS 2 GPG key
-sudo apt update && sudo apt install curl -y
+sudo apt update && sudo apt install -y curl
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 
 # Add the repository to the sources list
@@ -46,7 +46,7 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-a
 # Install ROS 2 Packages
 sudo apt update
 sudo apt upgrade -y
-sudo apt install ros-humble-desktop -y
+sudo apt install -y ros-humble-desktop
 echo "ROS 2 - Humble successfully installed"
 
 #### INSTALL PYTHON DEPENDENCIES ####
@@ -57,6 +57,9 @@ sudo apt install python3.10 python3.10-dev python3.10-venv -y
 
 # Install pip
 sudo apt -y install python3-pip
+
+# Install colcon
+sudo apt install python3-colcon-common-extensions -y
 
 # Install python packages
 sudo pip3 install -r requirements.txt
