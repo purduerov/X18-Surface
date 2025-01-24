f19839ca32c595b5bb26ffe546901ceebba08280
Icyadam14 <adamkahl8@gmail.com>
2025-01-24 18:10:51 +0000

    Update frontend to support socketio

---
+++ frontend/launch/surface_launch.py
@@ -0,0 +1,38 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import TimerAction
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='frontend',
+            executable='app.py',
+            namespace='rov',
+        ),
+        # Node(
+        #     package='controller',
+        #     executable='sender.py',
+        #     namespace='rov',
+        # ),
+        Node(
+            package='mediamtx_node',
+            executable='mediamtx_node.py',
+            namespace='rov',
+        ),
+        TimerAction(
+            period=5.0,  # Delay for n seconds
+            actions=[
+                Node(
+                    package='heartbeat_monitor',
+                    executable='heartbeat_monitor.py',
+                    namespace='rov',
+                ),
+                # Node(
+                #     package='demo_subscriber',
+                #     executable='demo_subscriber.py',
+                #     namespace='rov',
+                # )
+            ]
+        ),
+
+    ])
