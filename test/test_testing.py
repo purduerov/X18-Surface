5da5f29ad18d9c6f185de9ca0719569f4c28e96d
Intraleaf <mattmswang@gmail.com>
2025-11-05 20:57:00 -0500

    Prints heartbeats to terminal for CICD

---
+++ test/test_testing.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import time
+import unittest
+
+import launch
+import launch_ros
+import launch_testing.actions
+import rclpy
+from turtlesim.msg import Pose
+# basic imports and turtlesim apparently
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import TimerAction
+
+def generate_launch_description():
+    return LaunchDescription(
+        [
+            Node(
+                package="frontend",
+                executable="app.py",
+                namespace="rov",
+            ),
+            # Node(
+            #     package='controller',
+            #     executable='sender.py',
+            #     namespace='rov',
+            # ),
+            # Node(
+            #     package="mediamtx_node",
+            #     executable="mediamtx_node.py",
+            #     namespace="rov",
+            # ),
+            TimerAction(
+                period=5.0,  # Delay for n seconds
+                actions=[
+                    Node(
+                        package="heartbeat_monitor",
+                        executable="heartbeat_monitor.py",
+                        namespace="rov",
+                    ),
+                    Node(
+                        package='ui_subscriber',
+                        executable='ui_subscriber.py',
+                        namespace='rov',
+                    )
+                ],
+            ),
+        ]
+ 
