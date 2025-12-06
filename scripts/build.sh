943bc97a2f37faa0a8c708e45cfd3d59910a106a
Intraleaf <mattmswang@gmail.com>
2025-12-06 14:15:45 -0500

    builds all including tests now, depends on all packages required

--- scripts/build.sh
+++ scripts/build.sh
@@ -45,6 +45,7 @@ source /opt/ros/jazzy/setup.bash
 
 
 
-colcon build --cmake-args -DBUILD_TESTING=OFF
+colcon build 
+#--cmake-args -DBUILD_TESTING=OFF
 . install/setup.bash
 export ROS_DOMAIN_ID=69
